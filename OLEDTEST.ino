b0eb9f95c541a52fb95a047cf7cf11a899dbe6f5
Mag-netic <118333219+Mag-netic@users.noreply.github.com>
2023-10-16 00:13:56 -0600

    Add files via upload
    Updated Version of Oled Code
    -getButton Function now prints the proper character value depending on button press(it was a test)
    -switched the getButton if requirements to exact values instead of windows. Exact values are 0,202,408,1021
    -moved the main menu display and turned it into a function.
    -started messing around with state and stae variables.

--- OLEDTEST.ino
+++ OLEDTEST.ino
@@ -11,7 +11,7 @@
 #define rst 9     //res is Reset connected to output pin 9
 #define dc 8      //dc is data connected to output pin 8
 int button = A0;  //input pin Analog 0 is connected to 3 microswitches names button
-
+int LED = A3;
 // Color definitions
 #define BLACK 0x0000
 #define BLUE 0x001F
@@ -21,24 +21,25 @@ int button = A0;  //input pin Analog 0 is connected to 3 microswitches names but
 #define MAGENTA 0xF81F
 #define YELLOW 0xFFE0
 #define WHITE 0xFFFF
+Adafruit_SSD1331 display = Adafruit_SSD1331(cs, dc, rst);
 
-char getButton(int buttonRead) {
-  char buttonPressed = '0';
-  if (button <= 300 && button > 5) {
+char getButton(int button) {
+  char buttonPressed = 'z';
+  if (button == 202) {
     buttonPressed = 'a';
-  } else if (button > 300 && button <= 500) {
+  } else if (button == 408) {
     buttonPressed = 'b';
-  } else if (button > 500) {
+  } else if (button == 1021) {
     buttonPressed = 'c';
   }
+  Serial.println(buttonPressed);
   return buttonPressed;
 }
 
-Adafruit_SSD1331 display = Adafruit_SSD1331(cs, dc, rst);
-void setup() {
-  // put your setup code here, to run once:
-  display.begin();
-  display.fillScreen(BLUE);
+void getMainMenu (int screenState){ //gets the display setting for the Main Menu Screen
+  int state;
+  if(state == 0){
+    display.fillScreen(BLUE);
   display.setCursor(15, 15);
   display.setTextColor(YELLOW);
   display.setTextSize(2);
@@ -47,15 +48,45 @@ void setup() {
   display.setTextColor(YELLOW);
   display.setTextSize(2);
   display.print("Menu");
+  }
+}
+
+void getScreenOne(int screenState){
+  int state = screenState;
+  while(state == 1){
+    display.fillScreen(RED);
+    display.setCursor(15, 15);
+    display.setTextColor(WHITE);
+    display.setTextSize(2);
+    display.print("Menu");
+    display.setCursor(15, 40);
+    display.setTextColor(WHITE);
+    display.setTextSize(2);
+    display.print("ONE");
+  }
+}
+void setup() {
+  // put your setup code here, to run once:
+  display.begin();
+  int screenState = 0;
+  getMainMenu(screenState);
   Serial.begin(9600);  //sets the arduino to communicate with serial we set the range of 9600
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
   int buttonRead = analogRead(button);  //create an int to read the value of the button press
-  char buttonPressed = getButton(button);
-
+  Serial.println(buttonRead);
+  getButton(buttonRead);
+  Serial.println();
+  while(getButton(buttonRead) == 'a'){
+    int screenState = 1;
+    getScreenOne(screenState);
+  }
+  delay(600);
+  //char buttonPressed = getButton(button);
+  //Serial.println(buttonPressed);
+  
   /*Serial.println(buttonRead);  //prints the previously read button value to console
   if (buttonRead < 300) {
     int buttonPressed = 1;
@@ -68,7 +99,7 @@ void loop() {
     return (buttonPressed);
   }*/
 
-  if (buttonPressed = 'a') {
+  /*if (buttonPressed == 'a') {
     //display.begin();
     display.fillScreen(RED);
     display.setCursor(15, 15);
@@ -79,8 +110,8 @@ void loop() {
     display.setTextColor(WHITE);
     display.setTextSize(2);
     display.print("ONE");
-    delay(3000);
-  } else if (buttonPressed = 'b') {
+    //delay(3000);
+  } else if (buttonPressed == 'b') {
 
     display.fillScreen(GREEN);
     display.setCursor(15, 15);
@@ -91,8 +122,8 @@ void loop() {
     display.setTextColor(CYAN);
     display.setTextSize(2);
     display.print("TWO");
-    delay(3000);
-  } else if (buttonPressed = 'c') {
+    //delay(3000);
+  } else if (buttonPressed == 'c') {
 
     display.fillScreen(GREEN);
     display.setCursor(15, 15);
@@ -103,9 +134,9 @@ void loop() {
     display.setTextColor(BLACK);
     display.setTextSize(2);
     display.print("THREE");
-    delay(3000);
+    //delay(3000);
 
-  } else if (buttonPressed = '0'){
+  } else if (buttonPressed == '0'){
     display.fillScreen(BLUE);
     display.setCursor(15, 15);
     display.setTextColor(YELLOW);
@@ -115,6 +146,6 @@ void loop() {
     display.setTextColor(YELLOW);
     display.setTextSize(2);
     display.print("Menu");
-    delay(3000);
-  }
+    //delay(3000);
+  }*/
 }
