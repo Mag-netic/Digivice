ee737b6367e42aa2d1d9d7cc6cd90d301da3b5af
Mag-netic <118333219+Mag-netic@users.noreply.github.com>
2023-10-26 00:18:02 -0600

    Add files via upload
    Added button input with successful button debouncing. 
     -oled display still flickers
    -script not returning to original screen even when stating screen state

--- OLEDTEST.ino
+++ OLEDTEST.ino
@@ -5,13 +5,15 @@
 // This is a 96w x 64L display
 // You can use any (4 or) 5 pins
 // pins 13,11,10,9 & 8 is connected to OLED display
-#define sclk 13   //sclk is clock connected to output pin 13
-#define mosi 11   //
-#define cs 10     //cs is chip select connected to output pin 10
-#define rst 9     //res is Reset connected to output pin 9
-#define dc 8      //dc is data connected to output pin 8
-int button = A0;  //input pin Analog 0 is connected to 3 microswitches names button
-int LED = A3;
+#define sclk 13         //sclk is clock connected to output pin 13
+#define mosi 11         //
+#define cs 10           //cs is chip select connected to output pin 10
+#define rst 9           //res is Reset connected to output pin 9
+#define dc 8            //dc is data connected to output pin 8
+const int BUTTONA = 7;  //Sets Button A to pin 7
+const int BUTTONB = 4;  // Sets Button B to Pin 8
+const int BUTTONC = 5;  // Sets Button C to Pin 9
+
 // Color definitions
 #define BLACK 0x0000
 #define BLUE 0x001F
@@ -23,19 +25,70 @@ int LED = A3;
 #define WHITE 0xFFFF
 Adafruit_SSD1331 display = Adafruit_SSD1331(cs, dc, rst);
 
-char getButton(int button) {
-  char buttonPressed = 'z';
-  if (button >= 150 && button <= 250) {
-    buttonPressed = 'a';
-  } else if (button >= 300 && button <= 500) {
-    buttonPressed = 'b';
-  } else if (button >= 900 && button < 1200) {
-    buttonPressed = 'c';
+char getButton() {
+  char buttonPressed;                      //empty variable for button character
+  int buttonARead = digitalRead(BUTTONA);  // variable that reads input from pin
+  int buttonBRead = digitalRead(BUTTONB);
+  int buttonCRead = digitalRead(BUTTONC);
+  int buttonState;
+  int lastButtonState;
+  int buttonReadOld = 0;  //variable set to default button state
+  if (buttonARead != buttonReadOld) {
+    delay(150);
+    buttonState = 1;
+    if (buttonState = 1) {
+      buttonPressed = 'a';
+      Serial.println(buttonPressed);
+    }
+    return buttonPressed;
   }
-  Serial.println(buttonPressed);
-  return buttonPressed;
-  delay(2000);
+
+    if (buttonBRead != buttonReadOld) {
+    delay(150);
+    buttonState = 1;
+    if (buttonState = 1) {
+      buttonPressed = 'b';
+      Serial.println(buttonPressed);
+    }
+    return buttonPressed;
+  }
+
+    if (buttonCRead != buttonReadOld) {
+    delay(150);
+    buttonState = 1;
+    if (buttonState = 1) {
+      buttonPressed = 'c';
+      Serial.println(buttonPressed);
+    }
+    return buttonPressed;
+  }
 }
+
+/*char getButtonA() {
+  char buttonPressed;
+  int BUTTONANEW = digitalRead(BUTTONA);
+  int BUTTONBNEW = digitalRead(BUTTONA);
+  int BUTTONCNEW = digitalRead(BUTTONA);
+  int BUTTONAOLD;
+  int BUTTONBOLD;
+  int BUTTONCOLD;
+  if (BUTTONAOLD == 0 && BUTTONANEW == 1) {
+    buttonPressed = 'a';
+    BUTTONAOLD = BUTTONANEW;
+    delay(500);
+    return buttonPressed;
+  } else if (BUTTONBOLD == 0 && BUTTONBNEW == 1) {
+    buttonPressed = 'b';
+    BUTTONBOLD = BUTTONBNEW;
+    delay(500);
+    return buttonPressed;
+  } else if (BUTTONCOLD == 0 && BUTTONCNEW == 1) {
+    buttonPressed = 'c';
+    BUTTONCOLD = BUTTONCNEW;
+    delay(500);
+    return buttonPressed;
+  }
+}*/
 
 void getCharacterScreen(int screenState) {
   int state;
@@ -49,29 +102,20 @@ void getCharacterScreen(int screenState) {
     display.setTextColor(YELLOW);
     display.setTextSize(1);
     display.print("Screen");
-    int buttonRead = analogRead(button);  //create an int to read the value of the button press
-    Serial.println(buttonRead);           //prints the value of buttonRead
-    getButton(buttonRead);
 
-    switch (getButton(buttonRead)) {
+    //prints the value of buttonRead
+    getButton();
+    switch (getButton()) {
       case 'a':
         while (int screenState = 2) {
           getStatScreen(screenState);
-          getButton(buttonRead);
         }
         break;
       case 'b':
         while (int screenState = 14) {
           getConnectScreen(screenState);
-          getButton(buttonRead);
         }
         break;
-        /*case 'c':
-  while (int screenState = 1){
-  getScreenThree(screenState);
-  getButton(buttonRead);
-  break;
-  }*/
     }
   }
 }
@@ -88,29 +132,20 @@ void getStatScreen(int screenState) {
     display.setTextColor(WHITE);
     display.setTextSize(1);
     display.print("Screen");
-    int buttonRead = analogRead(button);  //create an int to read the value of the button press
-    Serial.println(buttonRead);           //prints the value of buttonRead
-    getButton(buttonRead);
-
-    switch (getButton(buttonRead)) {
+    getButton();
+    switch (getButton()) {
       case 'a':
         while (int screenState = 4) {
           getTrainingScreen(screenState);
-          getButton(buttonRead);
+          getButton();
         }
         break;
       case 'b':
         while (int screenState = 0) {
           getCharacterScreen(screenState);
-          getButton(buttonRead);
+          getButton();
         }
         break;
-        /*case 'c':
-  while (int screenState = 1){
-  getScreenThree(screenState);
-  getButton(buttonRead);
-  break;
-  }*/
     }
   }
 }
@@ -127,29 +162,20 @@ void getTrainingScreen(int screenState) {
     display.setTextColor(BLACK);
     display.setTextSize(1);
     display.print("Screen");
-    int buttonRead = analogRead(button);
-    Serial.println(buttonRead);
-    getButton(buttonRead);
-
-    switch (getButton(buttonRead)) {
+    getButton();
+    switch (getButton()) {
       case 'a':
         while (int screenState = 6) {
           getFoodScreen(screenState);
-          getButton(buttonRead);
+          getButton();
         }
         break;
       case 'b':
         while (int screenState = 2) {
           getStatScreen(screenState);
-          getButton(buttonRead);
+          getButton();
         }
         break;
-        /*case 'c':
-  while (int screenState = 1){
-  getScreenThree(screenState);
-  getButton(buttonRead);
-  break;
-  }*/
     }
   }
 }
@@ -166,29 +192,20 @@ void getFoodScreen(int screenState) {
     display.setTextColor(BLACK);
     display.setTextSize(1);
     display.print("Screen");
-    int buttonRead = analogRead(button);
-    Serial.println(buttonRead);
-    getButton(buttonRead);
-
-    switch (getButton(buttonRead)) {
+    getButton();
+    switch (getButton()) {
       case 'a':
         while (int screenState = 8) {
           getBattleScreen(screenState);
-          getButton(buttonRead);
+          getButton();
         }
         break;
       case 'b':
         while (int screenState = 4) {
           getTrainingScreen(screenState);
-          getButton(buttonRead);
+          getButton();
         }
         break;
-        /*case 'c':
-  while (int screenState = 1){
-  getScreenThree(screenState);
-  getButton(buttonRead);
-  break;
-  }*/
     }
   }
 }
@@ -205,29 +222,20 @@ void getBattleScreen(int screenState) {
     display.setTextColor(BLACK);
     display.setTextSize(1);
     display.print("Screen");
-    int buttonRead = analogRead(button);
-    Serial.println(buttonRead);
-    getButton(buttonRead);
-
-    switch (getButton(buttonRead)) {
+    getButton();
+    switch (getButton()) {
       case 'a':
         while (int screenState = 10) {
           getRestScreen(screenState);
-          getButton(buttonRead);
+          getButton();
         }
         break;
       case 'b':
         while (int screenState = 6) {
           getFoodScreen(screenState);
-          getButton(buttonRead);
+          getButton();
         }
         break;
-        /*case 'c':
-  while (int screenState = 1){
-  getScreenThree(screenState);
-  getButton(buttonRead);
-  break;
-  }*/
     }
   }
 }
@@ -244,28 +252,20 @@ void getRestScreen(int screenState) {
     display.setTextColor(BLACK);
     display.setTextSize(1);
     display.print("Screen");
-    int buttonRead = analogRead(button);
-    Serial.println(buttonRead);
-    getButton(buttonRead);
-    switch (getButton(buttonRead)) {
+    getButton();
+    switch (getButton()) {
       case 'a':
         while (int screenState = 12) {
           getHealScreen(screenState);
-          getButton(buttonRead);
+          getButton();
         }
         break;
       case 'b':
         while (int screenState = 8) {
           getBattleScreen(screenState);
-          getButton(buttonRead);
+          getButton();
         }
         break;
-        /*case 'c':
-  while (int screenState = 1){
-  getScreenThree(screenState);
-  getButton(buttonRead);
-  break;
-  }*/
     }
   }
 }
@@ -282,28 +282,20 @@ void getHealScreen(int screenState) {
     display.setTextColor(BLACK);
     display.setTextSize(1);
     display.print("Screen");
-    int buttonRead = analogRead(button);
-    Serial.println(buttonRead);
-    getButton(buttonRead);
-    switch (getButton(buttonRead)) {
+    getButton();
+    switch (getButton()) {
       case 'a':
         while (int screenState = 14) {
           getConnectScreen(screenState);
-          getButton(buttonRead);
+          getButton();
         }
         break;
       case 'b':
         while (int screenState = 10) {
           getRestScreen(screenState);
-          getButton(buttonRead);
+          getButton();
         }
         break;
-        /*case 'c':
-  while (int screenState = 1){
-  getScreenThree(screenState);
-  getButton(buttonRead);
-  break;
-  }*/
     }
   }
 }
@@ -320,66 +312,33 @@ void getConnectScreen(int screenState) {
     display.setTextColor(BLACK);
     display.setTextSize(1);
     display.print("Screen");
-    int buttonRead = analogRead(button);
-    Serial.println(buttonRead);
-    getButton(buttonRead);
-    switch (getButton(buttonRead)) {
+    getButton();
+    switch (getButton()) {
       case 'a':
         while (int screenState = 0) {
           getCharacterScreen(screenState);
-          getButton(buttonRead);
+          getButton();
         }
         break;
       case 'b':
         while (int screenState = 12) {
           getHealScreen(screenState);
-          getButton(buttonRead);
+          getButton();
         }
         break;
-        /*case 'c':
-  while (int screenState = 1){
-  getScreenThree(screenState);
-  getButton(buttonRead);
-  break;
-  }*/
     }
   }
 }
 void setup() {
   // put your setup code here, to run once:
+  pinMode(BUTTONA, INPUT);  //set button as input
+  pinMode(BUTTONB, INPUT);
+  pinMode(BUTTONC, INPUT);
   display.begin();
-  Serial.begin(9600);  //sets the arduino to communicate with serial we set the range of 9600
+  Serial.begin(9600);  //sets the arduino to communicate with serial port
 }
 
 void loop() {
   int screenState = 0;
   getCharacterScreen(screenState);
-
-  // put your main code here, to run repeatedly:
-  //int buttonRead = analogRead(button);  //create an int to read the value of the button press
-  //Serial.println(buttonRead);           //prints the value of buttonRead
- // getButton(buttonRead);                //a function that takes the buttonRead int input and returns a char value
-
-  /*switch (getButton(buttonRead)) {
-    case 'a':
-      while (int screenState = 2) {
-        getStatScreen(screenState);
-        getButton(buttonRead);
-      }
-      break;
-    case 'b':
-      while (int screenState = 14) {
-        getConnectScreen(screenState);
-        getButton(buttonRead);
-      }
-      break;
-      case 'c':
-  while (int screenState = 1){
-  getScreenThree(screenState);
-  getButton(buttonRead);
-  break;
-  }
-  }*/
-
-  delay(600);
 }
