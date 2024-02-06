bcef977e1260e406fdc30242e98f450a68db5665
Mag-netic <118333219+Mag-netic@users.noreply.github.com>
2024-02-05 22:01:44 -0700

    Add files via upload
    Updated stable version
    Switched to a different 96x64 pixel tft display with built in sd card reader. 
    successfully utilized the sd card reader and display to read and draw images utilizing the image reader library.
    buttons working.

---
+++ DPetV2.ino
@@ -0,0 +1,114 @@
+
+#include <Adafruit_GFX.h>         // Core graphics library
+#include <Adafruit_SSD1331.h>      // Hardware-specific library
+#include <SdFat.h>                // SD card & FAT filesystem library
+#include <Adafruit_SPIFlash.h>    // SPI / QSPI flash library
+#include <Adafruit_ImageReader.h> // Image-reading functions
+
+// Comment out the next line to load from SPI/QSPI flash instead of SD card:
+#define USE_SD_CARD
+
+// Color definitions
+#define BLACK           0x0000
+#define BLUE            0x001F
+#define RED             0xF800
+#define GREEN           0x07E0
+#define CYAN            0x07FF
+#define MAGENTA         0xF81F
+#define YELLOW          0xFFE0
+#define WHITE           0xFFFF
+
+// TFT display and SD card share the hardware SPI interface, using
+// 'select' pins for each to identify the active device on the bus.
+
+#define TFT_CS  10 // TFT select pin
+#define TFT_RST  9 // Or set to -1 and connect to Arduino RESET pin
+#define TFT_DC   8 // TFT display/command pin
+#define SD_CS    4 // SD card select pin
+const int BUTTONA = 7;  //Sets Button A to pin 7
+const int BUTTONB = 6;  // Sets Button B to Pin 6
+const int BUTTONC = 5;  // Sets Button C to Pin 5
+#if defined(USE_SD_CARD)
+  SdFat                SD;         // SD card filesystem
+  Adafruit_ImageReader reader(SD); // Image-reader object, pass in SD filesys
+#endif
+
+
+Adafruit_SSD1331 tft = Adafruit_SSD1331(&SPI, TFT_CS, TFT_DC, TFT_RST);
+Adafruit_Image       img;        // An image loaded into RAM
+int32_t              width  = 0, // BMP image dimensions
+                     height = 0;
+
+char getButton() {
+  char buttonPressed;                      //empty variable for button character
+  int buttonARead = digitalRead(BUTTONA);  // variable that reads input from pin
+  int buttonBRead = digitalRead(BUTTONB);
+  int buttonCRead = digitalRead(BUTTONC);
+  int buttonState;
+  int lastButtonState;
+  int buttonReadOld = 0;  //variable set to default button state
+  if (buttonARead != buttonReadOld) {
+    delay(180);
+    buttonState = 1;
+    if (buttonState == 1) {
+      buttonPressed = 'a';
+      Serial.println(buttonPressed);
+    }
+    return buttonPressed;
+  }
+
+  if (buttonBRead != buttonReadOld) {
+    delay(180);
+    buttonState = 1;
+    if (buttonState == 1) {
+      buttonPressed = 'b';
+      Serial.println(buttonPressed);
+    }
+    return buttonPressed;
+  }
+
+  if (buttonCRead != buttonReadOld) {
+    delay(160);
+    buttonState = 1;
+    if (buttonState == 1) {
+      buttonPressed = 'c';
+      Serial.println(buttonPressed);
+    }
+    return buttonPressed;
+  }
+}
+
+void setup(void) {
+
+  ImageReturnCode stat; // Status from image-reading functions
+
+  Serial.begin(9600);
+#if !defined(ESP32)
+  while(!Serial);       // Wait for Serial Monitor before continuing
+#endif
+
+  tft.begin(); // Initialize screen
+
+  // The Adafruit_ImageReader constructor call (above, before setup())
+  // accepts an uninitialized SdFat or FatVolume object. This MUST
+  // BE INITIALIZED before using any of the image reader functions!
+  Serial.print(F("Initializing filesystem..."));
+#if defined(USE_SD_CARD)
+  // SD card is pretty straightforward, a single call...
+  if(!SD.begin(SD_CS, SD_SCK_MHZ(10))) { // Breakouts require 10 MHz limit due to longer wires
+    Serial.println(F("SD begin() failed"));
+    for(;;); // Fatal error, do not continue
+  }
+#endif
+  Serial.println(F("OK!"));
+  stat = reader.drawBMP("/charm.bmp", tft, 0, 0);
+}
+
+void loop() {
+  pinMode(BUTTONA, INPUT);  //set button as input
+  pinMode(BUTTONB, INPUT);
+  pinMode(BUTTONC, INPUT);
+  getButton();
+  reader.drawBMP("/charm.bmp", tft, 0, 0);
+}
+
